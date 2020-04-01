6bd9d894e8951d3da908babce94d6b3347ed8925
ljcmps01 <ljcmps01@gmail.com>
2020-04-01 20:49:56 -0300

    C codes to edit long sentences according to OLED display sizes

---
+++ Manejo-de-strings.c
@@ -0,0 +1,12 @@
+//#include <iostream.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void){
+    int msg=123;
+    printf("Introduzca mensaje\n");
+    scanf("%d",msg);
+    printf("El mensaje introducido es %d\n",msg);
+    system("PAUSE");
+    return 0;
+}
