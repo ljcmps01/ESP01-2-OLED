552e9d883e31876168a716404b44f7e043679e1d
ljcmps01 <ljcmps01@gmail.com>
2020-04-01 21:43:21 -0300

    OTA compatible sketch for ESP01

---
+++ OLED-ESP01-OTA/OLED-ESP01-OTA.ino
@@ -0,0 +1,105 @@
+/*Control de OLED con ESP01
+ * Conexion WiFi y programación por OTA
+ 
+ *ESP01- pinout (conexión con display) 
+  7-     8-VCC
+  5-SDA  6-Rst (VCC)
+  3-SCL  4-CE(VCC)
+  1-GND  2-
+*/
+#include <ss_oled.h>
+#include "ESPHelper.h"
+
+#define USE_BACKBUFFER
+
+#ifdef USE_BACKBUFFER
+static uint8_t ucBackBuffer[1024];
+#else
+static uint8_t *ucBackBuffer = NULL;
+#endif
+#define SDAp 0
+#define SCLp 2
+
+netInfo homeNet = {  .mqttHost = "",     //can be blank if not using MQTT
+          .mqttUser = "",   //can be blank
+          .mqttPass = "",   //can be blank
+          .mqttPort = 1883,         //default port for MQTT is 1883 - only chance if needed.
+          .ssid = "Campos", 
+          .pass = "perico15"};
+
+ESPHelper myESP(&homeNet);
+
+void setup() {
+int rc;
+// The I2C SDA/SCL pins set to -1 means to use the default Wire library
+// If pins were specified, they would be bit-banged in software
+// This isn't inferior to hw I2C and in fact allows you to go faster on certain CPUs
+// The reset pin is optional and I've only seen it needed on larger OLEDs (2.4")
+//    that can be configured as either SPI or I2C
+//
+
+//   oledInit(type, rotate180, invert, SDA_PIN, SCL_PIN, RESET_PIN, speed)
+rc = oledInit(OLED_128x64, 0, 0, SDAp, SCLp, -1, 400000L); // use standard I2C bus at 400Khz
+  if (rc != OLED_NOT_FOUND)
+  {
+    char *msgs[] = {(char *)"SSD1306 @ 0x3C", (char *)"SSD1306 @ 0x3D",(char *)"SH1106 @ 0x3C",(char *)"SH1106 @ 0x3D"};
+    oledFill(0, 1);
+    oledWriteString(0,0,0,msgs[rc], FONT_NORMAL, 0, 1);
+    oledSetBackBuffer(ucBackBuffer);
+    delay(2000);
+  }
+  oledFill(0,1);
+  oledWriteString(0,0,0,(char *)"Setting up OTA", FONT_NORMAL,1,1);
+myESP.OTA_enable();
+myESP.OTA_setPassword("123");
+myESP.OTA_setHostname("ESP01-A");
+myESP.begin();
+  oledWriteString(0,0,2,(char *)"OTA ready!!", FONT_LARGE,0,1);
+  oledSetBackBuffer(ucBackBuffer);
+  oledFill(0x0, 1);
+  delay(1000);
+
+} /* setup() */
+
+void loop() {
+  myESP.loop();  //run the loop() method as often as possible - this keeps the network services running
+  // put your main code here, to run repeatedly:
+int i, x, y;
+String mensaje="Prueba Alejo";
+String msg= "Hola probando las string en el ESP-01";
+int caracteres=msg.length(), Div=caracteres/21,rde=3-Div/2;
+String renglon[8];
+  myESP.loop();
+for(int i=0;i<=Div;i++){
+  renglon[i]=msg.substring(21*i,21+21*i);
+  myESP.loop();
+}
+unsigned long ms;
+  myESP.loop();
+  
+  oledWriteString(0,16,0,(char *)"ss_oled Demo", FONT_NORMAL, 0, 1);
+  oledWriteString(0,0,1,(char *)mensaje.c_str(), FONT_SMALL, 1, 1);
+  oledWriteString(0,0,5,(char *)"1237890", FONT_SMALL, 0, 1);
+  oledWriteString(0,0,6,(char *)"11111111111111111111", FONT_SMALL, 0, 1);
+  oledWriteString(0,0,7,(char *)"place", FONT_SMALL, 0, 1);
+  oledWriteString(0,32,7,(char *)"place", FONT_SMALL, 0, 1);
+  oledWriteString(0,99,7,(char *)"place", FONT_SMALL, 0, 1);
+  
+  delay(1000);
+  oledSetBackBuffer(ucBackBuffer);
+  oledFill(0x0,1);
+  myESP.loop();  //run the loop() method as often as possible - this keeps the network services running
+  delay(1000);
+  for(int i=0;i<=Div;i++){
+  myESP.loop();
+  oledWriteString(0,0,rde+i,(char *)renglon[i].c_str(), FONT_SMALL, 0, 1);
+  delay(750);
+  }
+  delay(2000);
+  oledFill(0x0,1);
+  myESP.loop();
+  delay(1000);
+  
+  yield();
+
+} /* loop() */
