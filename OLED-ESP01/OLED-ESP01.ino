758289bfce4d8dc503138600e6718b18b49ac2e2
ljcmps01 <ljcmps01@gmail.com>
2020-04-01 21:18:13 -0300

    Sketch basico funcional (No WiFi)

--- OLED-ESP01/OLED-ESP01.ino
+++ OLED-ESP01/OLED-ESP01.ino
@@ -1,13 +1,11 @@
-//
-// Small Simple OLED library demo
-//
-// Demonstrates how to initialize and use a few functions of the ss_oled library
-// If your MCU has enough RAM, enable the backbuffer to see a demonstration
-// of the speed difference between drawing directly on the display versus
-// deferred rendering, followed by a "dump" of the memory to the display
-//
+/*Testeo de control de OLED con ESP01 basico (NoWiFi)
+  7-     8-VCC
+  5-SDA  6-Rst (VCC)
+  3-SCL  4-CE(VCC)
+  1-GND  2-
+*/
 #include <ss_oled.h>
-#include "ESPHelper.h"
+
 // if your system doesn't have enough RAM for a back buffer, comment out
 // this line (e.g. ATtiny85)
 #define USE_BACKBUFFER
@@ -18,15 +16,6 @@ static uint8_t ucBackBuffer[1024];
 static uint8_t *ucBackBuffer = NULL;
 #endif
 
-netInfo homeNet = {  .mqttHost = "",     //can be blank if not using MQTT
-          .mqttUser = "",   //can be blank
-          .mqttPass = "",   //can be blank
-          .mqttPort = 1883,         //default port for MQTT is 1883 - only chance if needed.
-          .ssid = "Campos", 
-          .pass = "perico15"};
-
-ESPHelper myESP(&homeNet);
-
 void setup() {
 int rc;
 // The I2C SDA/SCL pins set to -1 means to use the default Wire library
@@ -36,7 +25,6 @@ int rc;
 //    that can be configured as either SPI or I2C
 //
 // oledInit(type, rotate180, invert, SDA_PIN, SCL_PIN, RESET_PIN, speed)
-
 rc = oledInit(OLED_128x64, 0, 0, 0, 2, -1, 400000L); // use standard I2C bus at 400Khz
   if (rc != OLED_NOT_FOUND)
   {
@@ -46,58 +34,86 @@ rc = oledInit(OLED_128x64, 0, 0, 0, 2, -1, 400000L); // use standard I2C bus at 
     oledSetBackBuffer(ucBackBuffer);
     delay(2000);
   }
-  oledFill(0,1);
-  oledWriteString(0,0,0,(char *)"Setting up OTA", FONT_NORMAL,1,1);
-myESP.OTA_enable();
-myESP.OTA_setPassword("123");
-myESP.OTA_setHostname("ESP01-A");
-myESP.begin();
-  oledWriteString(0,0,2,(char *)"OTA ready!!", FONT_LARGE,0,1);
-  oledSetBackBuffer(ucBackBuffer);
-  oledFill(0x0, 1);
-  delay(1000);
-
 } /* setup() */
 
 void loop() {
-  myESP.loop();  //run the loop() method as often as possible - this keeps the network services running
   // put your main code here, to run repeatedly:
 int i, x, y;
-String mensaje="Prueba Alejo";
-String msg= "Hola probando las string en arduino";
-int caracteres=msg.length(), Div=caracteres/21,rde=3-Div/2;
-String renglon[8];
-  myESP.loop();
-for(int i=0;i<=Div;i++){
-  renglon[i]=msg.substring(21*i,21+21*i);
-  myESP.loop();
-}
+char szTemp[32];
 unsigned long ms;
-  myESP.loop();
-  
-  oledWriteString(0,16,0,(char *)"ss_oled Demo", FONT_NORMAL, 0, 1);
-  oledWriteString(0,0,1,(char *)mensaje.c_str(), FONT_SMALL, 1, 1);
-  oledWriteString(0,0,5,(char *)"1237890", FONT_SMALL, 0, 1);
-  oledWriteString(0,0,6,(char *)"11111111111111111111", FONT_SMALL, 0, 1);
-  oledWriteString(0,0,7,(char *)"place", FONT_SMALL, 0, 1);
-  oledWriteString(0,32,7,(char *)"place", FONT_SMALL, 0, 1);
-  oledWriteString(0,99,7,(char *)"place", FONT_SMALL, 0, 1);
-  
-  delay(1000);
-  oledSetBackBuffer(ucBackBuffer);
-  oledFill(0x0,1);
-  myESP.loop();  //run the loop() method as often as possible - this keeps the network services running
-  delay(1000);
-  for(int i=0;i<=Div;i++){
-  myESP.loop();
-  oledWriteString(0,0,rde+i,(char *)renglon[i].c_str(), FONT_SMALL, 0, 1);
-  delay(750);
-  }
-  delay(2000);
-  oledFill(0x0,1);
-  myESP.loop();
-  delay(1000);
-  
-  yield();
 
+  oledFill(0x0, 1);
+  oledWriteString(0,16,0,(char *)"ss_oled Demo", FONT_NORMAL, 0, 1);
+  oledWriteString(0,0,1,(char *)"Written by Larry Bank", FONT_SMALL, 1, 1);
+  oledWriteString(0,0,3,(char *)"**Demo**", FONT_LARGE, 0, 1);
+  delay(2000);
+  
+ // Pixel and line functions won't work without a back buffer
+#ifdef USE_BACKBUFFER
+  oledFill(0, 1);
+  oledWriteString(0,0,0,(char *)"Backbuffer Test", FONT_NORMAL,0,1);
+  oledWriteString(0,0,1,(char *)"3000 Random dots", FONT_NORMAL,0,1);
+  delay(2000);
+  oledFill(0,1);
+  ms = millis();
+  for (i=0; i<3000; i++)
+  {
+    x = random(128);
+    y = random(64);
+    oledSetPixel(x, y, 1, 1);
+  }
+  ms = millis() - ms;
+  sprintf(szTemp, "%dms", (int)ms);
+  oledWriteString(0,0,0,szTemp, FONT_NORMAL, 0, 1);
+  oledWriteString(0,0,1,(char *)"Without backbuffer", FONT_SMALL,0,1);
+  delay(2000);
+  oledFill(0,1);
+  ms = millis();
+  for (i=0; i<3000; i++)
+  {
+    x = random(128);
+    y = random(64);
+    oledSetPixel(x, y, 1, 0);
+  }
+  oledDumpBuffer(NULL);
+  ms = millis() - ms;
+  sprintf(szTemp, "%dms", (int)ms);
+  oledWriteString(0,0,0,szTemp, FONT_NORMAL, 0, 1);
+  oledWriteString(0,0,1,(char *)"With backbuffer", FONT_SMALL,0,1);
+  delay(2000);
+  oledFill(0, 1);
+  oledWriteString(0,0,0,(char *)"Backbuffer Test", FONT_NORMAL,0,1);
+  oledWriteString(0,0,1,(char *)"96 lines", FONT_NORMAL,0,1);
+  delay(2000);
+  ms = millis();
+  for (x=0; x<127; x+=2)
+  {
+    oledDrawLine(x, 0, 127-x, 63, 1);
+  }
+  for (y=0; y<63; y+=2)
+  {
+    oledDrawLine(127,y, 0,63-y, 1);
+  }
+  ms = millis() - ms;
+  sprintf(szTemp, "%dms", (int)ms);
+  oledWriteString(0,0,0,szTemp, FONT_NORMAL, 0, 1);
+  oledWriteString(0,0,1,(char *)"Without backbuffer", FONT_SMALL,0,1);
+  delay(2000);
+  oledFill(0,1);
+  ms = millis();
+  for (x=0; x<127; x+=2)
+  {
+    oledDrawLine(x, 0, 127-x, 63, 0);
+  }
+  for (y=0; y<63; y+=2)
+  {
+    oledDrawLine(127,y, 0,63-y, 0);
+  }
+  oledDumpBuffer(ucBackBuffer);
+  ms = millis() - ms;
+  sprintf(szTemp, "%dms", (int)ms);
+  oledWriteString(0,0,0,szTemp, FONT_NORMAL, 0, 1);
+  oledWriteString(0,0,1,(char *)"With backbuffer", FONT_SMALL,0,1);
+  delay(2000);
+#endif
 } /* loop() */
